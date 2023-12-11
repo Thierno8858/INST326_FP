1a300c92c1699bfaf78d6b8e2e09c3b2edb6591d
bilal nyle zafar <bnz1013@gmail.com>
2023-12-11 13:49:02 -0500

    Revised boss fight with enemy hp and counterattacks

--- fp_code.py/bossfight.py
+++ fp_code.py/bossfight.py
@@ -1,87 +1,79 @@
-class Bossfight:
-    def __init__(self):
-        self.boss = boss 
+import random
+import pandas as pd
+import seaborn as sns
+
+class Bossfight(Character):
+    def __init__(self, boss_name="Finale Projectus", boss_health=25):
         super().__init__()
+        self.boss_name = boss_name
+        self.boss_health = boss_health
+
+    def attack_boss(self):
+        """
+        Attack the boss and calculate damage. Generates random number between 1 and 7 for # of damage dealt
+        """
+        damage = random.randint(1, 7)  # Random damage between 1 and 7
+        print(f"You attack the boss dealing {damage} damage.")
+        self.boss_health -= damage
+
+        if self.boss_health <= 0:
+            print(f"You have defeated {self.boss_name}!")
+            return True
+        else:
+            print(f"{self.boss_name} has {self.boss_health} health remaining.")
+            return False
+    
+    def boss_counterattack(self):
+        """
+        Boss makes a counterattack with a 1 in 3 chance.
+        """
+        if random.randint(1, 3) == 1:  # 1 in 3 chance
+            print(f"The {self.boss_name} counterattacks!")
+            self.lose_life()
+            if self.lives > 0:
+                print(f"You have {self.lives} lives remaining.")
+            else:
+                print("You have been defeated by the boss!")
+
+
     def battle_boss(self):
-        """
-        Start the boss battle. Prompt the user to attack the boss
-        """
-        print("You have encountered the biggest enemy of them all, prepare for battle!")
+        print(f"You have encountered {self.boss_name}, prepare for battle!")
         
-        while self.lives > 0:
-            print("\n--- Round Start ---")
-            self.display_stats()
-            
-            while True:
-                attack_input = input("Do you want to attack? (yes/no): ").lower()
-                if attack_input in ["yes", "no"]:
-                    break
-                else:
-                    print("Invalid input! Please enter 'yes' or 'no'.")
-        
-    class ChallengePanda(Character):
-    def __init__(self):
-        # Initialize the ChallengePanda by calling the parent class constructor
-        super().__init__()
-
-    def battle_panda(self):
-        """
-        Start the battle with a panda. Prompt the user to input difficulty and attack the panda.
-        """
-        print("You encounter a fierce panda! Get ready for battle.")
-
-        # Prompt user to choose difficulty with error handling
-        difficulty_input = input("Choose difficulty (easy, normal, hard): ")
-        self.difficulty = self._get_valid_difficulty(difficulty_input)
-        self.lives = self._set_starting_lives()
-
-        print(f"You chose {self.difficulty} difficulty. Starting with {self.lives} lives.")
-
-        # Battle the panda
-        while self.lives > 0:
+        while self.lives > 0 and self.boss_health > 0:
             print("\n--- Round Start ---")
             self.display_stats()
 
-            # Prompt user to attack with error handling
-            while True:
+            attack_input = input("Do you want to attack? (yes/no): ").lower()
+            while attack_input not in ["yes", "no"]:
+                print("Invalid input! Please enter 'yes' or 'no'.")
                 attack_input = input("Do you want to attack? (yes/no): ").lower()
-                if attack_input in ["yes", "no"]:
-                    break
-                else:
-                    print("Invalid input! Please enter 'yes' or 'no'.")
 
             if attack_input == "yes":
-                if self.attack():
-                    print("You defeated the panda!")
-                    challenge_choice = input("Do you want to take on another challenge for a chance to recive another life? (yes/no): ").lower()
-                    while challenge_choice not in ["yes", "no"]:
+                boss_defeated = self.attack_boss()
+                if boss_defeated:
+                    viz_input = input("Are you ready to visualize the scroll? (yes/no): ")
+                    while viz_input not in ["yes", "no"]:
                         print("Invalid input! Please enter 'yes' or 'no'.")
-                        challenge_choice = input("Do you want to take on another challenge for a chance to recive another life? (yes/no): ").lower()
+                    if viz_input == "yes":
+                        df = pd.read_csv("FPscroll.csv")
+                        plot = sns.scatterplot(x='col1', y='col2', data=df, color='red')
+                        plot.set_title('Great Job!')
+                        plot.set_ylabel('You Win!')
+                        plot.set_xlabel('You have defeated the enemies and escaped the computer!')
 
-                    if challenge_choice == "yes":
-                        # Assuming ObstacleCourse is defined and implemented 
-                        obstacle_course_challenge = ObstacleCourse()
-                        obstacle_course_challenge.run_obstacle_course()
-                        
-                        
-                        # Check if the challenge is successful and update lives
-                        #if obstacle_course_challenge.successful_challenge:
-                            #self.gain_life()
-                        #else:
-                            #self.lose_life()
-                            
-                            
-                    else:
-                        print("You decided not to take on another challenge.")
-                        break
+                    break
                 else:
-                    print("The panda counterattacks! You lose a life.")
-                    self.lose_life()
+                    if random.randint(1, 3) == 1:  # 1 in 3 chance
+                        print(f"The {self.boss_name} counterattacks!")
+                        self.lose_life()
+                    if self.lives > 0:
+                        print(f"You have {self.lives} lives remaining.")
+                    else:
+                        print("You have been defeated by the boss!")
             else:
-                print("You decide not to attack. The panda is unimpressed.")
-                break
+                print(f"You choose not to attack. {self.boss_name} is waiting.")
+    
+        if self.lives == 0:
+            print("Game Over! You have been defeated.")
 
-            if self.lives == 0:
-                print("Game Over! The panda has defeated you.")
-            else:
-                print(f"You have {self.lives} lives remaining.")
+
